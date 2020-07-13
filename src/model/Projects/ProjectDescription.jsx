1a6bcb4465d6ee9a324b88000c2e0c24ca156356
Dawid <dawid.ciechowski@gmail.com>
2020-07-13 22:37:01 +0100

    Fixed build errors

--- src/model/Projects/ProjectDescription.jsx
+++ src/model/Projects/ProjectDescription.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Banner from './banner.jpg';
 import './ProjectDescription.css';
-import Projects from './Projects';
 
 
 const ProjectDescription = ({ match }) => {
